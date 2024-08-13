5d470f6cc4f04f4a9626d6b0b3f962900b5a0c64
NoufalNach <noufalnachz@gmail.com>
2024-08-13 23:23:41 +0530

    save2

--- lib/main.dart
+++ lib/main.dart
@@ -3,106 +3,132 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp( MyApp());
-} 
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List myDatalist=[
+    List myDatalist = [
       {
-    "name": "Sky",
-    "dp": "https://images.pexels.com/photos/27593745/pexels-photo-27593745/free-photo-of-luan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    "bg": "https://images.pexels.com/photos/27630047/pexels-photo-27630047/free-photo-of-windows-autumn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-  },
-  {
-    "name": "Luna",
-    "dp": "https://images.pexels.com/photos/27626066/pexels-photo-27626066/free-photo-of-egret.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    "bg": "https://images.pexels.com/photos/27626066/pexels-photo-27626066/free-photo-of-egret.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-  },
-  {
-    "name": "Nova",
-    "dp": "https://images.pexels.com/photos/25255043/pexels-photo-25255043/free-photo-of-entrance-with-arc.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    "bg": "https://images.pexels.com/photos/25255043/pexels-photo-25255043/free-photo-of-entrance-with-arc.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-  },
-  {
-    "name": "Orion",
-    "dp": "https://images.pexels.com/photos/25244939/pexels-photo-25244939/free-photo-of-swoyambhu-mahachaitya-in-kathmandu-in-nepal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    "bg": "https://images.pexels.com/photos/25244939/pexels-photo-25244939/free-photo-of-swoyambhu-mahachaitya-in-kathmandu-in-nepal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-  },
-  {
-    "name": "Celeste",
-    "dp": "https://images.pexels.com/photos/27412350/pexels-photo-27412350/free-photo-of-a-living-room-with-a-couch-coffee-table-and-plants.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    "bg": "https://images.pexels.com/photos/27412350/pexels-photo-27412350/free-photo-of-a-living-room-with-a-couch-coffee-table-and-plants.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-  },
-      {"name":"Marie","dp":"https://images.pexels.com/photos/27593745/pexels-photo-27593745/free-photo-of-luan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load", "bg":"https://images.pexels.com/photos/27643534/pexels-photo-27643534/free-photo-of-glass-of-fruit-cocktail.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"}
+        "name": "Sky",
+        "dp":
+            "https://images.pexels.com/photos/27593745/pexels-photo-27593745/free-photo-of-luan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/27630047/pexels-photo-27630047/free-photo-of-windows-autumn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      },
+      {
+        "name": "Luna",
+        "dp":
+            "https://images.pexels.com/photos/27626066/pexels-photo-27626066/free-photo-of-egret.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/27626066/pexels-photo-27626066/free-photo-of-egret.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      },
+      {
+        "name": "Nova",
+        "dp":
+            "https://images.pexels.com/photos/27659135/pexels-photo-27659135/free-photo-of-a-woman-walking-down-the-street-in-front-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/27659135/pexels-photo-27659135/free-photo-of-a-woman-walking-down-the-street-in-front-of-a-restaurant.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      },
+      {
+        "name": "Orion",
+        "dp":
+            "https://images.pexels.com/photos/25244939/pexels-photo-25244939/free-photo-of-swoyambhu-mahachaitya-in-kathmandu-in-nepal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/25244939/pexels-photo-25244939/free-photo-of-swoyambhu-mahachaitya-in-kathmandu-in-nepal.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      },
+      {
+        "name": "Celeste",
+        "dp":
+            "https://images.pexels.com/photos/27412350/pexels-photo-27412350/free-photo-of-a-living-room-with-a-couch-coffee-table-and-plants.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/27412350/pexels-photo-27412350/free-photo-of-a-living-room-with-a-couch-coffee-table-and-plants.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      },
+      {
+        "name": "Marie",
+        "dp":
+            "https://images.pexels.com/photos/27593745/pexels-photo-27593745/free-photo-of-luan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "bg":
+            "https://images.pexels.com/photos/27643534/pexels-photo-27643534/free-photo-of-glass-of-fruit-cocktail.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      }
     ];
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: PageView.builder(
             itemCount: myDatalist.length,
-            itemBuilder:(context, index) => Container(
-              padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20 ),
-              decoration: BoxDecoration(image: DecorationImage(
-                fit: BoxFit.cover,image:NetworkImage(
-                  myDatalist[index]["bg"]
-
-                ))),
-
-
-            child: Column(children: [
-              Divider(thickness: 3,color: Colors.white,
-              ),
-              ListTile(leading: CircleAvatar(backgroundImage: NetworkImage(myDatalist[index]["bg"]),
-              ),title:Text(myDatalist[index]["name"],style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
-              ) ,
-              subtitle: Text("Username",
-              style: TextStyle(color: Colors.white,fontSize: 12),
-              ),
-              trailing: Icon(Icons.more_vert,color: Colors.white,),
-              ),
-              Spacer(),
-              Row(
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(myDatalist[index]["bg"]))),
+              child: Column(
                 children: [
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50  ),
-                    border: Border.all(
-                      color: Colors.white,width: 1.5, )
-                      ),
-                      child: Text("Send Message",
-                                style: TextStyle(color: Colors.white,
-                                fontSize: 14),
-                      ),
-                      
-                      ),
-                       
-                    
-                ),
-                SizedBox(width: 15,),
-                    Icon(Icons.favorite_outline,
+                  Divider(
+                    thickness: 3,
                     color: Colors.white,
-                    size: 40,
-                     
-                    
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(myDatalist[index]["bg"]),
                     ),
-                    Icon(Icons.send,
-                    color: Colors.white,
-                    size: 40,
-                    )
-
-                ],)
-            ],),   
-            ) ,
+                    title: Text(
+                      myDatalist[index]["name"],
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Username",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                    trailing: Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 1.5,
+                              )),
+                          child: Text(
+                            "Send Message",
+                            style: TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Icon(
+                        Icons.favorite_outline,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 40,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 }
-  
